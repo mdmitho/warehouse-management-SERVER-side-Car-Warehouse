397da7e050b8718c20df774bde1e8b33fb404def
Hossen <mdmitho199911@gmail.com>
2022-04-29 05:20:42 +0600

    sarver running done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors')
+const port = process.env.PORT || 5000
+const app= express()
+
+//middlewre
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+res.send('auto mobail running')
+})
+
+app.listen(port,()=>{
+    console.log('Auto Mobail running');
+})
