d450d6ca9486a502d32f53f54e31773b51d963e2
Hossen <mdmitho199911@gmail.com>
2022-05-04 02:17:08 +0600

    loas all car collection API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const res = require('express/lib/response');
 require('dotenv').config()
 const port = process.env.PORT || 4000
 const app= express()
@@ -14,13 +15,24 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8nvw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('car warehouse connected');
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+try{
+ await client.connect()
+ const carCollection = client.db("carwarehouse").collection("carcollection");
+app.get('/carcollection',async(req,res)=>{
+    const query ={}
+    const cursor = carCollection.find(query)
+    const car = await cursor.toArray();
+    res.send(car)
+})
+ 
+}
+finally{
 
+}
+
+}
+run().catch(console.dir)
 
 
 app.get('/',(req,res)=>{
