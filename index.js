9c5eb98169201811cc3dcda9fbffd89a7a8188d4
Hossen <mdmitho199911@gmail.com>
2022-05-04 01:55:09 +0600

    connect to database with secure passwoed

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors')
-const port = process.env.PORT || 5000
+require('dotenv').config()
+const port = process.env.PORT || 4000
 const app= express()
 
 //middlewre
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8nvw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('car warehouse connected');
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/',(req,res)=>{
